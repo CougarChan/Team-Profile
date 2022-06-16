c9d870d5d88e1c491d143bbd590e59d6f351e426
CougarChan <gohan0136@gmail.com>
2022-06-16 14:45:09 -0400

    pushed the first part of the prompts

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const Everyone =require ('./lib/Everyone');
+const inquirer =require ('inquirer');
+const FileCreate =require ('./src/File.Create');
+const fs =require ('fs');
+
+
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+
+
+
+function MakingTeam() {
+    inquirer
+    .prompt([
+    {   type:'list',
+        message: 'Which employee will you be hiring?',
+        name: "EmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+    ]).then(function(userInput) {
+        switch(userInput.addingEmployeePrompt) {
+            case "Manager": addingManager();
+            break;
+
+            case "Engineer": addingEngineer();
+            break;
+
+            case "Intern": addingIntern();
+            break;
+
+
+ 
+         // Making the html
+             
+            default:
+                htmlBuilder();
+        }
+    });
+};
