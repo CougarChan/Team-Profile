e0ac82cb456a30b7fb8b0288ec4f0f9f6ee8d71c
gohan0136 <93087137+CougarChan@users.noreply.github.com>
2022-06-18 11:07:15 -0400

    Update Intern.test.js

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,7 +1,12 @@
 const Intern = require('../lib/Intern'); 
 // Intern Test
 test('get intern school via method', () => {
+   
+    // Const for the school
     const school = 'University of Miami';
     const newIntern = new Intern(1, 'Cancer', 'gohan0136@gmail.com', school);
+    
+    //this is for the test and will expect this to be what goes throught the test
+    
     expect(newIntern.getSchool()).toBe(school)
 });
